2bab3b47ceae099b839ee71d16402b26e431ebc4
jiseok94 <wlghl0405@naver.com>
2022-05-17 15:19:16 +0900

    강사님 수정

--- newplanin/home/urls.py
+++ newplanin/home/urls.py
@@ -7,7 +7,7 @@ app_name = "home"
 urlpatterns = [
     path('', views.load_project_lists, name = "home"),
     path('create-project/', views.create_project, name="create"),
-    # path('edit-project/<str:pid>/',views.edit_project, name="edit"),
+    path('edit-project/<str:pid>/',views.edit_project, name="edit"),
     path('delete-project/<str:pid>/',views.delete_project, name="delete")
     # path('invite/<int:pid>/<str:user_email>',views.invite_users, name="invite_users")
 ]
