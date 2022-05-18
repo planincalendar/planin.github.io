7bb5af899b0db7926240937c6322c1b44dbd1e3e
Jhlee1012 <leejinhyun327@gmail.com>
2022-05-18 16:46:31 +0900

    mysql db 설정

--- newplanin/newplanin/settings.py
+++ newplanin/newplanin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,10 +85,19 @@ WSGI_APPLICATION = 'newplanin.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default" :{
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : os.getenv('DJANGO_DB_NAME','planin'),
+        'USER' : os.getenv('DJANGO_DB_USER','user'),
+        'PASSWORD' : os.getenv('DJANOGO_DB_PASS','user1234'),
+        'HOST' : os.getenv('DJANGO_DB_HOST','database-1.cbf2lc2jetbl.ap-northeast-2.rds.amazonaws.com'),
+        'PORT' : os.getenv('DJANGO_DB_POPT','3306')
     }
+
 }
 
 
